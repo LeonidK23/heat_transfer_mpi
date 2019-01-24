fc0645de19761bed9a196ec45d32f2feeb2dac13
leonid <kli.voyage@gmail.com>
2019-01-24 10:45:30 +0100

    Create grid and heat sources

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <random>
+
+#define N 8
+#define N_SOURCES 5
+#define SOURCE 100
+
+void print_grid(std::vector<std::vector<float>> grid){
+  int i, j;
+  int n_rows = grid.size();
+  int n_cols = grid[0].size();
+
+  for (i = 0; i < n_rows; i++)
+    for(j = 0; j < n_cols; j++)
+      std::cout << grid[i][j] << '\n';
+}
+
+int main() {
+  int i, j;
+  std::mt19937 gen(std::random_device{}());
+  std::uniform_real_distribution<> dis(0, N);
+  int rand_row, rand_col;
+  std::vector<std::vector<float>> grid(N);
+
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++){
+      grid[i].push_back(0);
+      std::cout << grid[i][j] << '\n';
+    }
+  }
+
+  for(i = 0; i < N_SOURCES; i++){
+    rand_row = dis(gen);
+    rand_col = dis(gen);
+    grid[rand_row][rand_col] = SOURCE;
+  }
+
+  print_grid(grid);
+
+  return 0;
+}
